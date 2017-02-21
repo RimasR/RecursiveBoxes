ea554f9836d65063f31bb5d8dfc44a88d45f19fe
Rimantas Radžiūnas <RIRAD@danskebank.lt>
2017-02-21 19:22:24 +0200

    Created project

--- RecursiveBoxes/Program.cs
+++ RecursiveBoxes/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RecursiveBoxes
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public List<List<int>> results = new List<List<int>>();
+        public static void Main(string[] args)
         {
+            List<int> boxList = new List<int>();
+
+            boxList.Add(8);
+            boxList.Add(2);
+            boxList.Add(3);
+            boxList.Add(5);
+            boxList.Add(9);
         }
+
+
     }
 }
