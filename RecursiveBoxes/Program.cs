03fd7ef22adf1c000dbd95f52a8826511bd22fdf
Rimas-PC <Rimas-PC@ADMINRG-2NJ1LRT>
2017-02-24 09:19:09 +0200

    Refactored for better reading

--- RecursiveBoxes/Program.cs
+++ RecursiveBoxes/Program.cs
@@ -7,39 +7,47 @@ namespace RecursiveBoxes
 {
     public class Program
     {
-        private static List<int> boxList;
-        private static int M = 20;
+        private static List<int> _boxList;
+        private static int _maxBoxVolume;
+
         public static void Main(string[] args)
         {
-            List<List<int>> possibleRes = new List<List<int>>();
-            int result = 0;
-            var tooBigList = new List<int>();
-            boxList = new List<int> { 8, 2, 3, 5, 9, 11, 5, 6, 7, 8, 1, 1, 1, 1, 14, 7, 3, 4, 9, 4 };
-            boxList = boxList.OrderByDescending(x => x).ToList();
+            _boxList = new List<int> { 8, 2, 4, 3, 5, 6, 1};
+            _maxBoxVolume = 10;
+            GetMinimumBoxes();
 
-            foreach (var i in boxList)
+        }
+
+        public static int GetMinimumBoxes()
+        {
+            List<List<int>> setOfResultSets = new List<List<int>>();
+            int result = 0;
+            var listOfExceedingVolumes = new List<int>();
+            _boxList = _boxList.OrderByDescending(x => x).ToList();
+
+            foreach (var boxVolume in _boxList)
             {
-                if (i > M)
+                if (boxVolume > _maxBoxVolume)
                 {
-                    tooBigList.Add(i);
+                    listOfExceedingVolumes.Add(boxVolume);
                 }
             }
 
-            foreach (var j in tooBigList)
+            foreach (var boxVolume in listOfExceedingVolumes)
             {
-                boxList.Remove(j);
+                _boxList.Remove(boxVolume);
             }
 
-            while (boxList.Count != 0)
+            while (_boxList.Count != 0)
             {
-                var rez = Sort(0, 0);
+                var subsetOfBoxVolumes = Sort(0, 0);
 
-                if (rez != null)
+                if (subsetOfBoxVolumes != null)
                 {
-                    possibleRes.Add(rez);
-                    foreach (int value in rez)
+                    setOfResultSets.Add(subsetOfBoxVolumes);
+                    foreach (int boxVolume in subsetOfBoxVolumes)
                     {
-                        boxList.Remove(value);
+                        _boxList.Remove(boxVolume);
                     }
                 }
 
@@ -47,45 +55,45 @@ namespace RecursiveBoxes
             }
 
 
-            foreach (var subset in possibleRes)
+            foreach (var subset in setOfResultSets)
             {
                 Console.Write("{ ");
-                foreach (var variable in subset)
+                foreach (var volume in subset)
                 {
-                    Console.Write($"{variable} ");
+                    Console.Write($"{volume} ");
                 }
                 Console.Write("},");
             }
 
             Console.WriteLine($"\r\nMinimum boxes needed: {result}");
 
+            return result;
         }
 
         public static List<int> Sort(int sum, int nextIndex)
         {
-
-            var nextElement = boxList.ElementAt(nextIndex);
+            var nextElement = _boxList.ElementAt(nextIndex);
             sum += nextElement;
 
-            if (sum == M)
+            if (sum == _maxBoxVolume)
             {
                 return new List<int> { nextElement };
             }
 
-            if (sum > M)
+            if (sum > _maxBoxVolume)
             {
                 return null;
             }
 
-            if (sum < M)
+            if (sum < _maxBoxVolume)
             {
-                for (int j = nextIndex + 1; j < boxList.Count; j++)
+                for (int index = nextIndex + 1; index < _boxList.Count; index++)
                 {
-                    var x = Sort(sum, j);
-                    if (x != null)
+                    var boxVolumeSubset = Sort(sum, index);
+                    if (boxVolumeSubset != null)
                     {
-                        x.Add(nextElement);
-                        return x;
+                        boxVolumeSubset.Add(nextElement);
+                        return boxVolumeSubset;
                     }
                 }
 
